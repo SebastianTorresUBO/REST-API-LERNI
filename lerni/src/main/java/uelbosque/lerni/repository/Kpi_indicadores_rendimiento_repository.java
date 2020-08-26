242c531d2e81dcd8d73e02fefc7c173a559c71b9
sebastian <ytorrese@unbosque.edu.co>
2020-08-25 23:41:27 -0500

    Se borran clases que ya no se utilizaran

--- lerni/src/main/java/uelbosque/lerni/repository/Kpi_indicadores_rendimiento_repository.java
+++
@@ -1,9 +0,0 @@
-package uelbosque.lerni.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import uelbosque.lerni.model.Kpi_indicadores_rendimiento;
-
-public interface Kpi_indicadores_rendimiento_repository extends JpaRepository<Kpi_indicadores_rendimiento, Long> {
-
-}
